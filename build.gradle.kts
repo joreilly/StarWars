49c933a7d82cfefc1767988cfa11379dc89a79df
John O'Reilly <oreillyj@gmail.com>
2021-07-13 21:44:54 +0100

    update Apollo to 3.0.0-alpha1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,14 @@ buildscript {
         google()
         mavenCentral()
     }
+
+    val kotlinVersion: String by project
+    println("kotlinVersion = $kotlinVersion")
+
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
-        classpath("com.android.tools.build:gradle:7.0.0-beta04")
-        classpath("com.apollographql.apollo:apollo-gradle-plugin:${Versions.apollo}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:7.0.0-beta05")
+        classpath("com.apollographql.apollo3:apollo-gradle-plugin:${Versions.apollo}")
     }
 }
 
