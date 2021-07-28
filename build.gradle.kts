d0c6b36d6ebe0b31b5c1079edcb78a1704169361
John O'Reilly <oreillyj@gmail.com>
2021-07-28 18:40:24 +0100

    Jetpack Compose 1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.android.tools.build:gradle:7.0.0-rc01")
+        classpath("com.android.tools.build:gradle:7.0.0")
         classpath("com.apollographql.apollo3:apollo-gradle-plugin:${Versions.apollo}")
     }
 }
