4ef850f01c1318d603b5252d47031627298848ba
John O'Reilly <oreillyj@gmail.com>
2021-12-28 23:32:52 +0000

    jvm test code updates

--- shared/src/jvmMain/kotlin/dev/johnoreilly/starwars/shared/Main.kt
+++ shared/src/jvmMain/kotlin/dev/johnoreilly/starwars/shared/Main.kt
@@ -1,17 +1,16 @@
 package dev.johnoreilly.starwars.shared
 
 import dev.johnoreilly.starwars.shared.di.initKoin
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     val koin = initKoin().koin
     val repo = koin.get<StarWarsRepository>()
 
-    repo.people.collect {
-        println(it)
-    }
+    val people = repo.people.first()
+    println(people)
 
-    repo.films.collect {
-        println(it)
-    }
+    val films = repo.films.first()
+    println(films)
 }
