2bd9d89bd7d57beea0a70e8e61e89b31f19299af
John O'Reilly <oreillyj@gmail.com>
2021-12-28 17:15:39 +0000

    test updates

--- shared/src/appleMain/kotlin/dev/johnoreilly/starwars/shared/di/KoiniOS.kt
+++ shared/src/appleMain/kotlin/dev/johnoreilly/starwars/shared/di/KoiniOS.kt
@@ -1,9 +1,10 @@
 package dev.johnoreilly.starwars.shared.di
 
+import com.apollographql.apollo3.cache.normalized.api.NormalizedCacheFactory
 import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
 import org.koin.dsl.module
 
 
 actual fun platformModule() = module {
-    single { SqlNormalizedCacheFactory("swapi.db") }
+    single<NormalizedCacheFactory> { SqlNormalizedCacheFactory("swapi.db") }
 }
