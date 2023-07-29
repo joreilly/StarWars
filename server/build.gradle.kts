35cf5b32989ab7b661e9af01ea1942bf4db46804
John O'Reilly <oreillyj@gmail.com>
2023-07-29 18:20:17 +0100

    update dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
   id("kotlin-platform-jvm")
-  id("org.jetbrains.kotlin.plugin.spring").version("1.6.10")
-  id("org.jetbrains.kotlin.plugin.serialization").version("1.6.10")
+  id("org.jetbrains.kotlin.plugin.spring").version("1.9.0")
+  id("org.jetbrains.kotlin.plugin.serialization").version("1.9.0")
   id("org.springframework.boot").version("3.0.5")
   id("com.google.cloud.tools.appengine").version("2.4.2")
 }
 
 
 dependencies {
-  implementation("com.expediagroup:graphql-kotlin-spring-server:6.4.0")
+  implementation("com.expediagroup:graphql-kotlin-spring-server:6.5.3")
   implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
-  testImplementation("com.squareup.okhttp3:okhttp:4.10.0")
+  testImplementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
 
 kotlin {
