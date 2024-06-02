8e666bad2f74fdbedfbfc82e917355cd077c0aa4
John O'Reilly <oreillyj@gmail.com>
2024-06-02 17:54:25 +0100

    fix broken test

--- shared/src/commonTest/kotlin/dev/johnoreilly/starwars/shared/StarWarsRepositoryTest.kt
+++ shared/src/commonTest/kotlin/dev/johnoreilly/starwars/shared/StarWarsRepositoryTest.kt
@@ -3,6 +3,8 @@ package dev.johnoreilly.starwars.shared
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.annotations.ApolloExperimental
 import com.apollographql.apollo3.api.toResponseJson
+import com.apollographql.apollo3.cache.normalized.api.MemoryCacheFactory
+import com.apollographql.apollo3.cache.normalized.normalizedCache
 import com.apollographql.apollo3.mockserver.MockResponse
 import com.apollographql.apollo3.mockserver.MockServer
 import dev.johnoreilly.starwars.shared.di.commonModule
@@ -35,29 +37,30 @@ class StarWarsRepositoryTest: KoinTest {
         }
     }
 
-    @Test
-    fun testStarWarsRepository() = runTest {
-        mockServerUrl = mockServer.url()
+@Test
+fun testStarWarsRepository() = runTest {
+    mockServerUrl = mockServer.url()
 
-        mockServer.enqueue(MockResponse.Builder().body(getAllPeopleMockResponse.toResponseJson()).build())
-        val people = repo.people.first()
-        assertEquals(2, people.size)
-        assertEquals("Person 1", people[0].name)
-        assertEquals("Home World 1", people[0].homeworld.name)
-        println(people)
+    mockServer.enqueue(MockResponse.Builder().body(getAllPeopleMockResponse.toResponseJson()).build())
+    val people = repo.people.first()
+    assertEquals(2, people.size)
+    assertEquals("Person 1", people[0].name)
+    assertEquals("Home World 1", people[0].homeworld.name)
+    println(people)
 
-        mockServer.enqueue(MockResponse.Builder().body(getAllFilmsMockResponse.toResponseJson()).build())
-        val films = repo.films.first()
-        assertEquals(2, films.size)
-        assertEquals("Film 1", films[0].title)
-        assertEquals("Director 1", films[0].director)
-        println(films)
-    }
+    mockServer.enqueue(MockResponse.Builder().body(getAllFilmsMockResponse.toResponseJson()).build())
+    val films = repo.films.first()
+    assertEquals(2, films.size)
+    assertEquals("Film 1", films[0].title)
+    assertEquals("Director 1", films[0].director)
+    println(films)
+}
 
     private fun createMockApolloClient(url: String): ApolloClient {
-        println("createMockApolloClient, ur = $url")
+        println("createMockApolloClient, url = $url")
         return ApolloClient.Builder()
             .serverUrl(url)
+            .normalizedCache(MemoryCacheFactory())
             .build()
     }
 }
