57af3d383ecf2929a067f9be5000f45b33411593
John O'Reilly <oreillyj@gmail.com>
2024-08-15 14:45:27 +0100

    offline apollo cache test

--- androidApp/src/main/java/dev/johnoreilly/starwars/androidApp/MainActivity.kt
+++ androidApp/src/main/java/dev/johnoreilly/starwars/androidApp/MainActivity.kt
@@ -7,44 +7,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import dev.johnoreilly.starwars.androidApp.theme.StarWarsTheme
-import dev.johnoreilly.starwars.fragment.FilmFragment
-import dev.johnoreilly.starwars.fragment.PersonFragment
 import dev.johnoreilly.starwars.shared.StarWarsRepository
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -53,162 +31,49 @@ class MainActivity : ComponentActivity() {
         setDecorFitsSystemWindows(window, false)
 
         setContent {
-            StarWarsTheme {
+            MaterialTheme {
                 MainLayout()
             }
         }
     }
 }
 
-sealed class Screen(val title: String) {
-    data object PersonList : Screen("Person List")
-    data object FilmList : Screen("Film List")
-}
-
-data class BottomNavigationItem(val route: String, val icon: Int, val iconContentDescription: String)
-
-val bottomNavigationItems = listOf(
-    BottomNavigationItem(Screen.PersonList.title, R.drawable.ic_face, Screen.PersonList.title),
-    BottomNavigationItem(Screen.FilmList.title, R.drawable.ic_movie, Screen.FilmList.title)
-)
-
 @Composable
 fun MainLayout() {
-    val navController = rememberNavController()
-    val repo = rememberStarWarsRepository()
+    val scope = rememberCoroutineScope()
+    val repo = remember { StarWarsRepository() }
 
-    val people by repo.people.collectAsState(emptyList())
-    val filmList by repo.films.collectAsState(emptyList())
+    val peopleData by repo.peopleData.collectAsState()
+    val filmData by repo.filmData.collectAsState()
 
     Scaffold(
-        topBar = { StarWarsTopAppBar("Star Wars") },
-        bottomBar = { StarWarsBottomNavigation(navController) }
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text("Star Wars") })
+        }
     ) {
         Column(Modifier.padding(it)) {
-            NavHost(navController, startDestination = Screen.PersonList.title) {
-                composable(Screen.PersonList.title) {
-                    PeopleList(people)
+            Row {
+                Button(onClick = {
+                    scope.launch { repo.clearData() }
+                }) {
+                    Text("Clear Data")
                 }
-                composable(Screen.FilmList.title) {
-                    FilmList(filmList)
+
+                Button(onClick = {
+                    scope.launch { getData(repo) }
+                }) {
+                    Text("Get Data")
                 }
             }
-        }
-    }
-}
 
-@Composable
-private fun rememberStarWarsRepository(): StarWarsRepository {
-    val repo = remember { StarWarsRepository() }
-    LaunchedEffect(Unit) {
-        repo.prefetch()
-    }
-    return repo
-}
-
-@Composable
-private fun StarWarsTopAppBar(title: String) {
-    CenterAlignedTopAppBar(
-        title = { Text(title) },
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = Color.Transparent
-        )
-    )
-}
-
-@Composable
-private fun StarWarsBottomNavigation(navController: NavHostController) {
-
-    NavigationBar(modifier = Modifier.navigationBarsPadding()) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-
-         bottomNavigationItems.forEach { item ->
-            NavigationBarItem(
-                icon = { Icon(painterResource(item.icon), contentDescription = item.iconContentDescription) },
-                selected = currentRoute == item.route,
-                onClick = {
-                    navController.navigate(item.route) {
-                        popUpTo(navController.graph.id)
-                        launchSingleTop = true
-                    }
-                }
-            )
-        }
-    }
-}
-
-const val PersonListTag = "PersonList"
-
-@Composable
-fun PeopleList(people: List<PersonFragment>) {
-    LazyColumn(modifier = Modifier.fillMaxSize().testTag(PersonListTag)) {
-        items(people) { person ->
-            PersonView(person)
-        }
-    }
-}
-
-@Composable
-fun PersonView(person: PersonFragment) {
-    Row(modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
-        ) {
-            ListItem(
-                headlineContent = {
-                    Text(
-                        person.name,
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                },
-                supportingContent = {
-                    Text(
-                        person.homeworld.name,
-                        style = MaterialTheme.typography.titleMedium,
-                        color = Color.DarkGray
-                    )
-                }
-            )
+            Text("Number of people = ${peopleData.size}")
+            Text("Number of films = ${filmData.size}")
         }
     }
 }
 
 
-const val FilmListTag = "FilmList"
-
-@Composable
-fun FilmList(filmList: List<FilmFragment>) {
-    LazyColumn(modifier = Modifier.fillMaxSize().testTag(FilmListTag)) {
-        items(items = filmList, itemContent = { film ->
-            FilmView(film)
-        })
-    }
-}
-
-
-@Composable
-fun FilmView(film: FilmFragment) {
-    Row(modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
-        ) {
-            ListItem(
-                headlineContent = { Text(film.title, style = MaterialTheme.typography.titleLarge) },
-                supportingContent = {
-                    Text(
-                        film.director,
-                        style = MaterialTheme.typography.titleMedium,
-                        color = Color.DarkGray
-                    )
-                }
-            )
-        }
-    }
+suspend fun getData(repo: StarWarsRepository) {
+    repo.collectPeopleInfo()
+    repo.collectFilmInfo()
 }
--- shared/src/commonMain/kotlin/dev/johnoreilly/starwars/shared/StarWarsRepository.kt
+++ shared/src/commonMain/kotlin/dev/johnoreilly/starwars/shared/StarWarsRepository.kt
@@ -1,48 +1,55 @@
 package dev.johnoreilly.starwars.shared
 
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.Query
-import com.apollographql.apollo.cache.normalized.watch
-import com.rickclephas.kmp.nativecoroutines.NativeCoroutineScope
-import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
+import com.apollographql.apollo.cache.normalized.FetchPolicy
+import com.apollographql.apollo.cache.normalized.fetchPolicy
 import dev.johnoreilly.starwars.GetAllFilmsQuery
 import dev.johnoreilly.starwars.GetAllPeopleQuery
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.flow.filter
+import dev.johnoreilly.starwars.fragment.FilmFragment
+import dev.johnoreilly.starwars.fragment.PersonFragment
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
-import okio.IOException
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class StarWarsRepository: KoinComponent {
-    @NativeCoroutineScope
-    val coroutineScope: CoroutineScope = MainScope()
-
     private val apolloClient: ApolloClient by inject()
 
-    @NativeCoroutines
-    val people = apolloClient.query(GetAllPeopleQuery()).watch()
-        .filter { it.exception == null }
-        .map { it.dataOrThrow().allPeople.people.mapNotNull { it?.personFragment }
+    val peopleData: MutableStateFlow<List<PersonFragment>> = MutableStateFlow(emptyList())
+    val filmData: MutableStateFlow<List<FilmFragment>> = MutableStateFlow(emptyList())
+
+    suspend fun clearData() {
+        peopleData.emit(emptyList())
+        filmData.emit(emptyList())
     }
 
-    @NativeCoroutines
-    val films = apolloClient.query(GetAllFilmsQuery()).watch()
-        .filter { it.exception == null }
-        .map { it.dataOrThrow().allFilms.films.mapNotNull { it?.filmFragment }
+    suspend fun collectPeopleInfo() {
+        apolloClient.query(GetAllPeopleQuery())
+            .fetchPolicy(FetchPolicy.CacheAndNetwork)
+            .toFlowV3()
+            .map { it.dataOrThrow().allPeople.people.mapNotNull { it?.personFragment } }
+            .catch {
+                println("JFOR: collectPeopleInfo, Exception $it")
+            }
+            .collect { data ->
+                peopleData.emit(data)
+                println("JFOR: collectPeopleInfo, Data $data")
+            }
     }
 
-    suspend fun prefetch() {
-        prefetch(GetAllPeopleQuery())
-        prefetch(GetAllFilmsQuery())
-    }
 
-    private suspend fun prefetch(query: Query<*>) {
-        try {
-            apolloClient.query(query).execute()
-        } catch (ioe: IOException) {
-            // ignore prefetch failure
-        }
+    suspend fun collectFilmInfo() {
+        apolloClient.query(GetAllFilmsQuery())
+            .fetchPolicy(FetchPolicy.CacheAndNetwork)
+            .toFlowV3()
+            .map { it.dataOrThrow().allFilms.films.mapNotNull { it?.filmFragment } }
+            .catch {
+                println("JFOR: collectFilmInfo, Exception $it")
+            }
+            .collect { data ->
+                filmData.emit(data)
+                println("JFOR: collectFilmInfo, Data $data")
+            }
     }
 }
