5ecb4be39116f09aa0fb95a4691a1920fd47f316
John O'Reilly <oreillyj@gmail.com>
2025-08-04 14:51:59 +0100

    bootRun + dependency updates

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,35 +1,15 @@
 plugins {
-  kotlin("multiplatform")
-  id("org.jetbrains.kotlin.plugin.spring").version("2.0.20")
-  id("org.jetbrains.kotlin.plugin.serialization").version("2.0.20")
-  id("org.springframework.boot").version("3.3.4")
+  alias(libs.plugins.kotlinJvm)
+  id("org.jetbrains.kotlin.plugin.spring").version("2.2.0")
+  id("org.jetbrains.kotlin.plugin.serialization").version("2.2.0")
+  id("org.springframework.boot").version("3.5.3")
 }
 
 
-kotlin {
-  jvm() {
-    withJava()
-  }
+dependencies {
+  implementation("com.expediagroup:graphql-kotlin-spring-server:8.8.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 
-  sourceSets {
-    val jvmMain by getting {
-      dependencies {
-        implementation("com.expediagroup:graphql-kotlin-spring-server:7.1.4")
-        implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-
-        //testImplementation("com.squareup.okhttp3:okhttp:4.11.0")
-      }
-    }
-  }
+  //testImplementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
-
-//appengine {
-//  stage {
-//    setArtifact(tasks.named("bootJar").flatMap { (it as Jar).archiveFile })
-//  }
-//  deploy {
-//    projectId = "star-wars"
-//    version = "GCLOUD_CONFIG"
-//  }
-//}
